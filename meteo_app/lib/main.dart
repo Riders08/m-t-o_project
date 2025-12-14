e0245387fe7b2f4b02a40ab6a790d7d6d14a0d4f
Lucas Rivoal <lucas.rivoal@etu.u-bordeaux.fr>
2025-12-14 17:09:27 +0100

    ajout formulaire choix de ville/pays

--- meteo_app/lib/main.dart
+++ meteo_app/lib/main.dart
@@ -40,27 +40,65 @@ class _MeteoAppState extends State<MeteoApp>  {
     }
   }
 
+  Future<void> _loadMeteofromSelectedValue(String value) async {
+    try{
+      final result = await _meteoServices.fetchMeteo(value);
+      setState(() {
+        _meteo = result;
+        _isloading = false;
+        _error = null;
+      });
+    }catch(e){
+      setState(() {
+        _error = "Ville introuvable";
+        _isloading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text('Météo')),
-        body: Center(
-          child: _isloading
-          ? const CircularProgressIndicator()
-          : _error != null
-            ? Text(_error!)
-            : Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(_meteo!.location, style: const TextStyle(fontSize: 24)),
-                Text('${_meteo!.temparature} °C', style: const TextStyle(fontSize: 40)),
-                Text(_meteo!.description),
-              ],
-            ),
-        )
-      )
+        body: 
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: 
+                [
+                  TextField(
+                    decoration: const InputDecoration(labelText: "Ville/Pays", border: OutlineInputBorder()),
+                    onSubmitted: (value) {
+                      setState(() {
+                        _isloading = true;
+                      });
+                      _loadMeteofromSelectedValue(value);
+                    }
+                  ), //Formulaire de demande de ville/pays
+
+                  const SizedBox(height: 20),
+
+                  Expanded(
+                    child: Center(
+                      child: _isloading
+                      ? const CircularProgressIndicator()
+                      : _error != null // Check internet et tous les autres problemes
+                        ? Text(_error!)
+                        : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: 
+                          [
+                            Text(_meteo!.location, style: const TextStyle(fontSize: 24)), // Lieu choisi ou par default
+                            Text('${_meteo!.temparature} °C', style: const TextStyle(fontSize: 40)), // température en Celsius
+                            Text(_meteo!.description), // Le temps sous sa forme
+                          ]),
+                    )
+                  ) // Resultat de base
+            ])
+          )
+      ),
     );
   }
 }
